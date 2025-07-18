3f4190326f2d588fde9383df27a8bb9fb48464f9
Rodrigo Cabezas <gitcabezas@gmail.com>
2025-07-18 11:44:44 -0300

    Organizando as prototipações e o frontend

--- src/login/login.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const loginForm = document.querySelector('form');
-    if (loginForm) {
-        loginForm.addEventListener('submit', (event) => {
-            event.preventDefault();
-            window.location.href = 'src/perfil/perfil.html';
-        });
-    }
-});
